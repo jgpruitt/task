d6351c179d18130794e6e97ffe32b4a0c9117aec
John Pruitt <jgpruitt@gmail.com>
2018-07-05 20:32:10 -0500

    finished a set of testable examples

--- example_test.go
+++ example_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jgpruitt/task"
 	"time"
 	"sync"
+	"sync/atomic"
 )
 
 func handlePanic(err interface{}, trace []*task.Frame) {
@@ -27,9 +28,30 @@ func ExampleRun_Synchronously() {
 	// I was blocked until it finished.
 }
 
+func ExampleRun_Asynchronously() {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	run := task.New(handlePanic)
+
+	run.Asynchronously(func() {
+		// do some hard work...
+		time.Sleep(5 * time.Second)
+		fmt.Println("I finished.")
+		wg.Done()
+	})
+
+	fmt.Println("I'm not waiting.")
+
+	// ok, NOW I'm waiting.
+	wg.Wait()
+
+	// Output:
+	// I'm not waiting.
+	// I finished.
+}
+
 func ExampleRun_At() {
-	// don't exit the example while we are waiting on
-	// the time to expire
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 
@@ -50,8 +72,6 @@ func ExampleRun_At() {
 }
 
 func ExampleRun_After() {
-	// don't exit the example while we are waiting on
-	// the time to expire
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 
@@ -69,4 +89,98 @@ func ExampleRun_After() {
 	// Output:
 	// This happens while waiting...
 	// Hello World!
+}
+
+func ExampleRun_Every() {
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+
+	run := task.New(handlePanic)
+
+	kill, _ := run.Every(1 * time.Second, func() {
+		fmt.Println("I ran.")
+		wg.Done()
+	})
+
+	fmt.Println("Waiting...")
+	wg.Wait()
+	kill()
+
+	// Output:
+	// Waiting...
+	// I ran.
+	// I ran.
+	// I ran.
+}
+
+func ExampleRun_Until() {
+	run := task.New(handlePanic)
+
+	every := 1 * time.Second
+	until := time.Now().Add(3 * time.Second)
+	run.Until(every, until, func() {
+		fmt.Println("I ran.")
+	})
+
+	fmt.Println("Sleeping...")
+	time.Sleep(5 * time.Second)
+
+	// Output:
+	// Sleeping...
+	// I ran.
+	// I ran.
+	// I ran.
+}
+
+func ExampleRun_Times() {
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+
+	run := task.New(handlePanic)
+
+	run.Times(1 * time.Second, 3, func() {
+		fmt.Println("I ran.")
+		wg.Done()
+	})
+
+	fmt.Println("Waiting...")
+	wg.Wait()
+
+	// Output:
+	// Waiting...
+	// I ran.
+	// I ran.
+	// I ran.
+}
+
+func ExampleRun_These() {
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+
+	var accum int32 = 0
+
+	run := task.New(handlePanic)
+
+	task1 := func() {
+		atomic.AddInt32(&accum, 1)
+		wg.Done()
+	}
+
+	task2 := func() {
+		atomic.AddInt32(&accum, 2)
+		wg.Done()
+	}
+
+	task3 := func() {
+		atomic.AddInt32(&accum, 3)
+		wg.Done()
+	}
+
+	run.These(task1, task2, task3)
+
+	wg.Wait()
+	fmt.Printf("accum: %d\n", accum)
+
+	// Output:
+	// accum: 6
 }
